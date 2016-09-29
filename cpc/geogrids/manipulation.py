e8b351b4adeb605ae336b29f0b2f9062ddb57056
Mike Charles <mike.charles@noaa.gov>
2016-09-29 13:50:56 -0400

    Add grid_to_stn function
    Migrate function from `data-utils` package that converts gridded data to station data. A station list in `cpc/geogrids/data` is included to accomplish this.

--- cpc/geogrids/manipulation.py
+++ cpc/geogrids/manipulation.py
@@ -9,6 +9,7 @@ import warnings
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 import mpl_toolkits.basemap
+from stats_utils.stats import find_nearest_index
 
 # This package
 from cpc.geogrids.exceptions import GeogridError
@@ -190,3 +191,47 @@ def smooth(data, grid, factor=0.5):
     data = gaussian_filter(data, factor, order=0, mode='nearest')
     # Reapply the mask from the initial data array
     return np.where(mask, np.NaN, data)
+
+
+def grid_to_stn(gridded_data, grid, stn_ids, stn_lats, stn_lons):
+    """Interpolates values from a grid to stations in a station list.
+
+    Parameters
+    ----------
+
+    - gridded_data (array_like)
+        - Array of gridded data
+    - grid (Grid)
+        - `data_utils.gridded.grid.Grid` that the gridded data is on
+    - stn_ids (list)
+        - List of station ids
+    - stn_lats (list)
+        - List of station lats
+    - stn_lons (list)
+        - List of station lons
+
+    Returns
+    -------
+
+    - array_like
+        - Array of values representing the value of the gridded data
+          interpolated to the station locations
+
+    Examples
+    --------
+
+    """
+    # Reshape gridded data to 2 dimensions if necessary
+    if gridded_data.ndim == 1:
+        gridded_data = np.reshape(gridded_data, (grid.num_y, grid.num_x))
+    # Create empty list to store station vals
+    stn_val = []
+    # Loop over all stations
+    for i in range(len(stn_ids)):
+        # Find closest grid point to station
+        x_index = find_nearest_index(grid.lons, stn_lons[i])
+        y_index = find_nearest_index(grid.lats, stn_lats[i])
+        # Assign station val
+        stn_val.append(gridded_data[y_index, x_index])
+
+    return stn_val
