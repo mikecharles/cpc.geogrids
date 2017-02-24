5581481fb72bc4ba0cdc9c6d6fe7fd9008d8b38e
Mike Charles <mike.charles@noaa.gov>
2017-02-24 14:46:19 -0500

    Fix name of 0.5 degree built-in grids
    The built-in grids starting with 0.5 should be "0.5deg-...", not "0.5-deg". This would make them consistent with the other grids, which have the number right next to the word "deg" (eg. 1deg-global).

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = ['scipy', 'cpc.stats']
 
 setup(
     name="cpc.geogrids",
-    version='v0.2.1',
+    version='v0.2.2',
     description="CPC geospatial grids (grid definitions, interpolation, etc.)",
     long_description=readme + '\n\n' + history,
     author="Mike Charles",
